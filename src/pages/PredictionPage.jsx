fb4903201e96052c6c8eaac53d743e8e8b87b769
arshad-v <arshadv4422@gmail.com>
2025-06-24 12:24:45 +0530

    feat: Point API to live Render backend

--- src/pages/PredictionPage.jsx
+++ src/pages/PredictionPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = 'https://absenteeism-backend-1.onrender.com';
 
 const PredictionPage = () => {
   const [formData, setFormData] = useState({
